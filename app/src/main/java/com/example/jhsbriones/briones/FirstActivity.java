3505dac09e28ec0bb76bf107bc66ff4af604d080
butchersix <jasperhavenbriones@gmail.com>
2018-12-06 21:47:58 +0800

    Show debug count for all button values

---
+++ app/src/main/java/com/example/jhsbriones/briones/FirstActivity.java
@@ -0,0 +1,23 @@
+package com.example.jhsbriones.briones;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+    public void startGame(View w) {
+        startActivity(new Intent(FirstActivity.this, SecondActivity.class));
+    }
+
+    public void quit(View v) {
+        finish();
+    }
+}
